b608b4e27dd12df2e8b2a2e9e6cce616f62f2137
Ronaldo <20222tadssaj0007@ifba.edu.br>
2026-05-20 21:35:12 -0300

    Firt test home screen

---
+++ app/src/main/java/com/example/fitworkup/ui/theme/home/Feature.kt
@@ -0,0 +1,8 @@
+package com.example.fitworkup.ui.home
+
+// Isso define o modelo de dados para o que é uma "Feature" (recurso)
+data class Feature(
+    val icon: String,
+    val title: String,
+    val desc: String
+)
